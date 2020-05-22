ef091e8298971f08636cae73fec321e2edc48dc3
Aaron Boult <aaronboult25@gmail.com>
2020-05-22 22:00:23 +0100

    Initial commit

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
-fn main() {
-    println!("Hello, world!");
+mod grid;
+mod solver;
+
+fn main(){
+
+    let mut soduku_grid = grid::generate_grid(26);
+
+    let mut solutions = solver::solve(&soduku_grid);
+
+    while solutions.is_err(){
+
+        soduku_grid = grid::generate_grid(26);
+        
+        solutions = solver::solve(&soduku_grid);
+
+    }
+
+    println!("{}", soduku_grid);
+
+    for sol in solutions.unwrap(){
+
+        println!("{}", sol);
+
+    }
+
 }
